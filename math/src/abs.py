b33d67a51a32438fbcbb04bad30676f01879e601
Tifanioh Randrianoelina <randrianoelinatifanioh@gmail.com>
2026-04-26 07:47:28 +0300

    feat : add new folder and new features

---
+++ math/src/abs.py
@@ -0,0 +1,31 @@
+'''
+In this program, I write the absolute value 
+For a number
+>> abs(-34) == 34
+
+To find a max absolute value in a list
+>> absMax([1,2,3,-12]) == 12
+
+To find a min absolute value in a list
+>> absMin([-1,2,3,-12]) == 1
+'''
+
+def abs(number):
+	if number > 0:
+		return number
+	else:
+		return -number
+	
+def absMax(list):
+	j = list[0]
+	for i in list:
+			if abs(i) > abs(j):
+				j = i
+	return j
+	
+def absMin(list):
+	j = list[0]
+	for i in list:
+		if abs(i) < abs(j):
+			j = i
+	return j
