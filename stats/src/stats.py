95babe806a851d92ca3cf7869a506d59a349fbde
Tifanioh Randrianoelina <randrianoelinatifanioh@gmail.com>
2026-04-28 05:47:21 +0300

    feat : add new library

---
+++ stats/src/stats.py
@@ -0,0 +1,30 @@
+'''
+This program représente the sum list
+'''
+from math_lib.src.sqrt import *
+
+def sum(L):
+	S = 0
+	n = len(L)
+	for i in range(n):
+		S += L[i]
+	return S
+	
+def mean(L):
+	S = 0
+	n = len(L)
+	for i in range(n):
+		S += L[i]
+	moy = S/n
+	return moy
+	
+def var(L):
+	S = 0
+	n = len(L)
+	for i in range(n):
+		S += L[i]**2
+	var = S/n - mean(L)**2
+	return var
+	
+def ecart(L):
+	return sqrt(var(L))
