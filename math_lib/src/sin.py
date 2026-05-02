9f8f2ef919823bf81a8b5c0e62794f093a46c724
Tifanioh Randrianoelina <randrianoelinatifanioh@gmail.com>
2026-05-02 20:52:10 +0300

    feat : create package sin

---
+++ math_lib/src/sin.py
@@ -0,0 +1,25 @@
+'''
+Function sinus implementation 
+'''
+def sin(angle, in_degrees=False): # in_degrees = degre
+    # 1.  Pi (approximation)
+    PI = 3.141592653589793
+    
+    # 2. Conversion
+    x = angle * (PI / 180) if in_degrees else angle
+    
+    # 3. Angle reduct (Modulo 2*PI)
+    x = x % (2 * PI)
+    
+    # 4. Calcul par série de Taylor optimisée
+    S = x
+    terme = x
+    f = 20     
+    
+    for i in range(1, f + 1):
+        terme *= -x**2 / ((2*i) * (2*i + 1))
+        S += terme
+        
+    return S
+
+print(sin(10, in_degrees=True))
