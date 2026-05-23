ff575edd6f43ff1d612e74092525b7c5e36cfda7
Tifanioh Randrianoelina <randrianoelinatifanioh@gmail.com>
2026-05-23 12:41:09 +0300

    feat: add tests

--- math_lib/tests/test_abs.py
+++ math_lib/tests/test_abs.py
@@ -2,16 +2,16 @@ import pytest
 from src.abs import *
 
 def test_abs():
-	assert abs(-2) == 2
-	assert abs(10) == 10
-	assert abs(-100) == 100
+	assert absolute.abs(-2) == 2
+	assert absolute.abs(10) == 10
+	assert absolute.abs(-100) == 100
 	
 def test_absMax():
-	assert absMax([1,4,6,-11]) == -11
-	assert absMax([1,6,7,25]) == 25
-	assert absMax([-1,-3,-7,-100]) == -100
+	assert absolute.absMax([1,4,6,-11]) == -11
+	assert absolute.absMax([1,6,7,25]) == 25
+	assert absolute.absMax([-1,-3,-7,-100]) == -100
 	
 def test_absMin():
-	assert absMin([-1,4,6,-11]) == -1
-	assert absMin([19,6,7,25]) == 6
-	assert absMin([-9,-3,-7,-100]) == -3
+	assert absolute.absMin([-1,4,6,-11]) == -1
+	assert absolute.absMin([19,6,7,25]) == 6
+	assert absolute.absMin([-9,-3,-7,-100]) == -3
