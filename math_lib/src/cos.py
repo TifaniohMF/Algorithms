837df6405dace34c7afb70f19cbb15920c036df3
Tifanioh Randrianoelina <randrianoelinatifanioh@gmail.com>
2026-05-23 13:47:22 +0300

    refactor: add class

--- math_lib/src/cos.py
+++ math_lib/src/cos.py
@@ -1,23 +1,24 @@
 '''
 Function cosinus implementation 
 '''
-def cos(angle, in_degrees=False): # in_degrees = degre
-    # 1.  Pi (approximation)
-    PI = 3.141592653589793
-    
-    # 2. Conversion
-    x = angle * (PI / 180) if in_degrees else angle
-    
-    # 3. Angle reduct (Modulo 2*PI)
-    x = x % (2 * PI)
-    
-    # 4. Calcul par série de Taylor optimisée
-    S = 1.0 
-    terme = 1.0
-    f = 20     
-    
-    for i in range(1, f + 1):
-        terme *= -x**2 / ((2*i - 1) * (2*i))
-        S += terme
-        
-    return S
+class cosinus:
+    def __init__(self, angle):
+    	self.angle = angle
+    def cos(angle, in_degrees=False): # in_degrees = degre
+    	# 1.  Pi (approximation)
+    	PI = 3.141592653589793
+    	
+    	# 2. Conversion
+    	x = angle * (PI / 180) if in_degrees else angle
+    	
+    	#3. Angle radian
+    	x = x % (2 * PI)
+    	
+    	# 4. Calcul by Taylor series optimised
+    	S = 1.0 
+    	terme = 1.0
+    	f = 20
+    	for i in range(1, f + 1):
+    	    terme *= -x**2 / ((2*i - 1) * (2*i))
+    	    S += terme
+    	return S
--- math_lib/tests/test_cos.py
+++ math_lib/tests/test_cos.py
@@ -1,7 +1,7 @@
 import pytest
-from src.cos import cos
+from src.cos import *
 
 def test_cos():
-	assert cos(0, in_degrees=True) == 1
-	assert cos(180, in_degrees=True) == -1
-	assert cos(5, in_degrees=True) == 0.9961946980917454
+	assert cosinus.cos(0, in_degrees=True) == 1
+	assert cosinus.cos(180, in_degrees=True) == -1
+	assert cosinus.cos(5, in_degrees=True) == 0.9961946980917454
