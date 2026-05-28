ced9e7636b793b1dabd700dcb3109c151c0bffa2
Tifanioh Randrianoelina <randrianoelinatifanioh@gmail.com>
2026-05-28 12:59:26 +0300

    refactor: add class

--- math_lib/src/abs.py
+++ math_lib/src/abs.py
@@ -4,30 +4,33 @@ For a number
 >> abs(-34) == 34
 
 To find a max absolute value in a list
->> absMax([1,2,3,-12]) == -12
+>> absMax([1,2,3,-12]) == 12
 
 To find a min absolute value in a list
->> absMin([-1,2,3,-12]) == -1
+>> absMin([-1,2,3,-12]) == 1
 '''
 
-class absolute(object):
-	def __init__(self, number, list):
-		self.number = number
-		self.list = list
-	def abs(number):
-	           if number > 0:
-	           	return number
-	           else:
-	           	return -number
-	def absMax(list):
-	    j = list[0]
-	    for i in list:
-	    	if abs(i) > abs(j):
-	    		j = i
-	    return j
-	def absMin(list):
-	     j = list[0]
-	     for i in list:
-	      	if abs(i) < abs(j):
-			      j = i
-	     return j
+class absolute:
+    def __init__(self, data):
+        self.number = data if isinstance(data, (int, float)) else None
+        self.liste= data if isinstance(data, list) else []
+
+    def abs(self):
+        if self.number > 0:
+            return self.number
+        else:
+            return -self.number
+
+    def absMax(self):
+        j = self.liste[0]
+        for i in self.liste:
+            if absolute(i).abs() > absolute(j).abs():
+                j = absolute(i).abs()
+        return j
+
+    def absMin(self):
+        j = self.liste[0]
+        for i in self.liste:
+            if absolute(i).abs() < absolute(j).abs():
+                j = absolute(i).abs()
+        return j
--- math_lib/tests/test_abs.py
+++ math_lib/tests/test_abs.py
@@ -2,16 +2,25 @@ import pytest
 from src.abs import *
 
 def test_abs():
-	assert absolute.abs(-2) == 2
-	assert absolute.abs(10) == 10
-	assert absolute.abs(-100) == 100
+	m1 = absolute(-2)
+	m2 = absolute(10)
+	m3 = absolute(-100)
+	assert m1.abs() == 2
+	assert m2.abs() == 10
+	assert m3.abs() == 100
 	
 def test_absMax():
-	assert absolute.absMax([1,4,6,-11]) == -11
-	assert absolute.absMax([1,6,7,25]) == 25
-	assert absolute.absMax([-1,-3,-7,-100]) == -100
+	m4 = absolute([1,4,6,-11])
+	m5 = absolute([1,6,7,25])
+	m6 = absolute([-1,-3,-7,-100])
+	assert m4.absMax() == 11
+	assert m5.absMax() == 25
+	assert m6.absMax() == 100
 	
 def test_absMin():
-	assert absolute.absMin([-1,4,6,-11]) == -1
-	assert absolute.absMin([19,6,7,25]) == 6
-	assert absolute.absMin([-9,-3,-7,-100]) == -3
+	m7 = absolute([-1,4,6,-11]) 
+	m8 = absolute([19,6,7,25])
+	m9 = absolute([-9,-3,10,-15])
+	assert m7.absMin() == -1
+	assert m8.absMin() == 6
+	assert m9.absMin() == 3
